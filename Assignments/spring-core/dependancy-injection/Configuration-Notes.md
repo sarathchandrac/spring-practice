90e059df12914ef080e41f6f4220d14710a743ca
Sarath Chandra <csc111vs@gmail.com>
2020-05-07 22:17:42 +0530

    add build parameters and read application-configuration.properties

---
+++ Assignments/spring-core/dependancy-injection/Configuration-Notes.md
@@ -0,0 +1,5 @@
+# External Configuration
+- Configuring external file:
+```shell script
+  java -jar app.jar --spring.config.location=file:///Users/home/config/application.properties
+```
--- Assignments/spring-core/dependancy-injection/src/main/java/org/spring/di/DependancyInjectionApplication.java
+++ Assignments/spring-core/dependancy-injection/src/main/java/org/spring/di/DependancyInjectionApplication.java
@@ -1,17 +1,29 @@
 package org.spring.di;
 
+import org.spring.di.configuration.MailConfig;
 import org.spring.di.controller.GreetingController;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
+@PropertySource("application-configuration.properties")
 public class DependancyInjectionApplication {
 	@Value("${welcome-message}")
 	private String welcomeMsg;
+	
+	@Autowired
+	private Environment environment;
+	
+	// Configuration Test
+	@Autowired
+	private MailConfig mailProperties;
 
 
 	public static void main(String[] args) {
@@ -22,8 +34,17 @@ public class DependancyInjectionApplication {
 	}
 	@Bean
 	public CommandLineRunner commandLineTest(){
-		return args -> System.out.println("args - " + welcomeMsg);
-
+		//return args -> System.out.println("args --> " + welcomeMsg);
+		return args -> {
+			System.out.println("args --> " + welcomeMsg);
+			System.out.println("args --> " + mailProperties);
+			mailProperties.getDefaultReciepients().forEach(System.out::println);
+			readEnvironmentInfo();
+		};
+	}
+	
+	public void readEnvironmentInfo() {
+		System.out.println("Environnet - path ---> " + environment.getProperty("path"));
 	}
 
 }
